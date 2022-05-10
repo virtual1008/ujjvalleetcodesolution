c30cce1d1ffdc17d1d4eabd22833e1f9b6fe8540
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2022-05-10 17:39:34 +0530

    Time: 8 ms (9.10%), Space: 14.3 MB (5.40%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
-    void fun(vector<vector<int>> & ans,vector<int> v,int i,int sum,int n,int k){
-        if(  i==10)
-        {
-            if(v.size()==k && sum==n)
-            {
-                    ans.push_back(v);
-                
-            }
-            return ;
+    void fun(set<vector<int>> & ans,vector<int> v,int i,int sum,int n,int k){
+        if(i==10){
+            if(v.size()==k && sum==n){
+            ans.insert(v);
         }
-        fun(ans,v,i+1,sum,n,k);
+            return;
+        }
+        if(v.size()==k && sum==n){
+            ans.insert(v);
+        }
         v.push_back(i);
         sum+=i;
-        
         fun(ans,v,i+1,sum,n,k);
+        sum-=i;
+        v.pop_back();
+        fun(ans,v,i+1,sum,n,k); 
         
         
     }
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<vector<int>> ans;
+        set<vector<int>> ans;
         vector<int> v;
         fun(ans,v,1,0,n,k);
-        return ans;
+        vector<vector<int>> res;
+        for(auto it:ans) res.push_back(it);
+        return res;
     }
 };
