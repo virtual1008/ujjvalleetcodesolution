5578c0453840c498a7efb0d32b9a689f17f402de
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2022-05-17 17:29:13 +0530

    Time: 807 ms (31.39%), Space: 165.9 MB (5.16%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
@@ -1,23 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
 class Solution {
-    
-    void dfs(TreeNode* og, TreeNode* copy, TreeNode* target, TreeNode*& ans) {
-        if(!og) return;
-        
-        if(og == target) {
-            ans = copy;
-        }
-        
-        dfs(og->left, copy->left, target, ans);
-        dfs(og->right, copy->right, target, ans);
-        
-        return;
-    }
-    
 public:
+    int getval(TreeNode* root,TreeNode* target){
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int n=q.size();
+            for(int i=0;i<n;i++){
+                auto it=q.front();
+                q.pop();
+                if(it==target) return it->val;
+                if(it->left) q.push(it->left);
+                if(it->right) q.push(it->right);
+            }
+        }
+        return root->val;
+    }
     TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
-        
-        TreeNode* ans = nullptr;
-        dfs(original, cloned, target, ans);
-        return ans;
+         int value=getval(original,target);
+          queue<TreeNode*> q;
+          TreeNode* root=cloned;
+          q.push(root);
+          while(!q.empty()){
+            int n=q.size();
+            for(int i=0;i<n;i++){
+                auto root=q.front();
+                q.pop();
+                if(root->val==value) return root;
+                if(root->left) q.push(root->left);
+                if(root->right) q.push(root->right);
+            }
+        }
+        return root;
+         
     }
 };
