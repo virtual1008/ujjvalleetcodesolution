2b190589f20f7ca0fd28de5944d7ed8b248b2502
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2022-05-24 01:41:05 +0530

    Time: 23 ms (82.72%), Space: 16.4 MB (69.27%) - LeetHub

--- 155-min-stack/155-min-stack.cpp
+++ 155-min-stack/155-min-stack.cpp
@@ -1,47 +1,35 @@
-class MinStack {
+class MinStack 
+{
 public:
-    stack<int>s;
-    priority_queue<int,vector<int>,greater<int>>q;
-    map<int,int>m;
-    MinStack() {
+    
+    stack<int> s1, s2;
+    
+    MinStack() 
+    {
         
     }
     
-    void push(int val) {
-        s.push(val);
-        q.push(val);
-        m[val]++;
+    void push(int val) 
+    {
+        s1.push(val);
+        if (s2.empty() || val <= s2.top())
+            s2.push(val);
     }
     
-    void pop() {
-        int val=s.top();
-        m[val]--;
-        s.pop();
+    void pop() 
+    {
+        if(s1.top() <= s2.top())
+            s2.pop();
+        s1.pop();
     }
     
-    int top() {
-        return s.top();
+    int top() 
+    {
+        return s1.top();
     }
     
-    int getMin() {
-        
-        stack<int>temp;
-        int mi=INT_MAX;
-        while(1)
-        {
-            int val=q.top();
-            if(m[val]>0)
-                return val;
-            q.pop();
-        }
+    int getMin() 
+    {
+        return s2.top();
     }
 };
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack* obj = new MinStack();
- * obj->push(val);
- * obj->pop();
- * int param_3 = obj->top();
- * int param_4 = obj->getMin();
- */
