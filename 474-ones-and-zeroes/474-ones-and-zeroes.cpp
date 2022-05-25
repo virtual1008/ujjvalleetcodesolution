a6c36a072dcd124b6ab966d61eaa0a344e147e86
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2022-05-25 11:00:37 +0530

    Time: 585 ms (21.26%), Space: 36.1 MB (31.10%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -19,28 +19,19 @@ public:
     int solve(int i, int one, int zero, int& maxZero, int& maxOne, 
              vector<string>& arr)
     {
-        if(i >= arr.size()) 
-            return 0;
-        if(one > maxOne || zero > maxZero)
-            return 0;
-        if(dp[i][one][zero] != -1)
-        {
-            return dp[i][one][zero];
+        if(i>=arr.size()) return 0;
+        auto it=countOneAndZero(arr[i]);
+        if(dp[i][one][zero]!=-1) return dp[i][one][zero];
+        int ans1,ans2,ans;
+        if(one+it.first<=maxOne && zero+it.second<=maxZero){
+            ans1=1+solve(i+1,one+it.first,zero+it.second,maxZero,maxOne,arr);
+             ans2=solve(i+1,one,zero,maxZero,maxOne,arr);
+            ans=max(ans1,ans2);
+        }else{
+            ans=solve(i+1,one,zero,maxZero,maxOne,arr);
         }
+        return dp[i][one][zero]=ans;
         
-        pair<int, int> p = countOneAndZero(arr[i]);
-        int ans1 = 0, ans2 = 0, ansWithout = 0;
-        if(one + p.first <= maxOne && zero + p.second <= maxZero)
-        {
-            ans1 = 1 + solve(i + 1, one + p.first, zero + p.second, 
-                           maxZero, maxOne, arr);
-            ans2 = solve(i + 1, one, zero, maxZero, maxOne, arr);
-        }
-        else 
-        {
-            ansWithout = solve(i + 1, one, zero, maxZero, maxOne, arr);
-        }
-        return dp[i][one][zero] = max({ans1, ans2, ansWithout});
         
         
     }
