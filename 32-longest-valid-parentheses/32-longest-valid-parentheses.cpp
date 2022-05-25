0d6c8357c2e0249ab573e5485889c3ab1fcc5a6b
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2022-05-25 11:28:47 +0530

    Time: 3 ms (89.02%), Space: 6.7 MB (99.58%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        stack<int> st;
-        st.push(-1);
         int maxi=0;
-        for(int i=0;i<s.length();i++){
-            if(s[i]=='(') st.push(i);
-            else{
-                st.pop();
-                if(st.empty()) st.push(i);
-                else{
-                    maxi=max(maxi,i-st.top());
-                }
+        int left=0,right=0;
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            if(s[i]=='(') left++;
+            else if(s[i]==')') right++;
+            if(left==right) maxi=max(maxi,left+right);
+            else if(right>left){
+                left=0;
+                right=0;
+            }
+        }
+        left=0,right=0;
+        for(int i=n-1;i>=0;i--){
+            if(s[i]==')') left++;
+            else if(s[i]=='(') right++;
+            if(left==right) maxi=max(maxi,left+right);
+            else if(right>left){
+                left=0;
+                right=0;
             }
         }
         return maxi;
