852721a2389f0f65f35fc662933e19759167affb
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2022-05-25 12:55:43 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (10.12%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int rec(int n,vector<int>&dp){
         if(n==0 || n==1) return n;
-        if(dp[n]!=-1) return dp[n];
+        if(dp[n]!=0) return dp[n];
         return dp[n]=rec(n-1,dp)+rec(n-2,dp);
     }
     int fib(int n) {
-        vector<int> dp(n+1,-1);
+        vector<int> dp(n+1,0);
         return rec(n,dp);
     }
 };
