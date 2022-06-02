50921e03879e330dbef695f08631ab821a534d1b
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2022-06-02 09:01:30 +0530

    Time: 15 ms (65.95%), Space: 10.7 MB (43.03%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.cpp
+++ 867-transpose-matrix/867-transpose-matrix.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
-    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
-        int n=matrix.size();
-        int m=matrix[0].size();
-        vector<vector<int>> ans;
-        for(int i=0;i<m;i++){
-            vector<int> temp;
-           for(int j=0;j<n;j++){
-              temp.push_back(matrix[j][i]);
-           }
-            ans.push_back(temp);
-        }
-        return ans;
-    }
+  vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+    vector<vector<int>> newMatrix(matrix[0].size(), vector<int>(matrix.size(), 0));
+    for(int i = 0; i < matrix.size(); i++)
+      for(int j = 0; j < matrix[0].size(); j++)
+        newMatrix[j][i] = matrix[i][j];
+
+    return newMatrix;
+  }
 };
