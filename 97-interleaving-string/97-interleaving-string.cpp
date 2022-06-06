c9ffe50e2cc7f1cd681a382dc5e0f38aec0b8022
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2022-06-06 16:07:45 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (76.75%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-
-int dp[101][101];
-bool solve(string &s1,string &s2,string &s3,int i,int j,int k,int n,int m,int sz){
-    if(i==n && j==m && k==sz) return true;
-    if(dp[i][j]!=-1) return dp[i][j];
-    bool c = false;
-    if(i<n){
-        if(s1[i]==s3[k]){
-            c = c || solve(s1,s2,s3,i+1,j,k+1,n,m,sz);
+    int dp[102][102];
+    bool solve(string &s1, string &s2, string &s3, int i, int j, int k){
+        
+        if(k >= s3.size()) 
+            return true;
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        bool flag1 = false, flag2 = false;
+        if(s1[i] == s3[k]){
+            flag1 =  solve(s1, s2, s3, i + 1, j, k + 1);
         }
-    }
-    if(j<m){
-        if(s2[j]==s3[k]){
-            c = c || solve(s1,s2,s3,i,j+1,k+1,n,m,sz);
+        
+        if(s2[j] == s3[k]){
+            flag2 =  solve(s1, s2, s3, i, j + 1, k + 1);
         }
+    
+        return dp[i][j] = (flag1 || flag2);
+        
     }
-    return dp[i][j] = c;
-}
-bool isInterleave(string s1, string s2, string s3) {
-    memset(dp,-1,sizeof(dp));
-    if(s1.size()+s2.size()!=s3.size()) return 0;
-    return solve(s1,s2,s3,0,0,0,s1.size(),s2.size(),s3.size());
-}
+    bool isInterleave(string s1, string s2, string s3) {
+         if((s1.size() + s2.size()) != s3.size()) return false;
+         memset(dp, -1,sizeof(dp));         
+         return solve(s1, s2, s3, 0 , 0 , 0);
+    }
 };
