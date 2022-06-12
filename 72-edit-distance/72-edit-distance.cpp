de34526e1535e955adf07ec2849e9d35b02f7941
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2022-06-12 18:22:05 +0530

    Time: 15 ms (68.89%), Space: 8.9 MB (64.27%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -11,7 +11,15 @@ public:
     int minDistance(string s1, string s2) {
         int n=s1.size();
         int m=s2.size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return rec(n-1,m-1,s1,s2,dp);
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        for(int i=0;i<=n;i++) dp[i][0]=i;
+        for(int i=0;i<=m;i++) dp[0][i]=i;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                dp[i][j]=dp[i-1][j-1];
+                 if(s1[i-1]!=s2[j-1])dp[i][j]=1+min(dp[i][j],min(dp[i-1][j],dp[i][j-1]));
+            }
+        }
+        return dp[n][m];
     }
 };
