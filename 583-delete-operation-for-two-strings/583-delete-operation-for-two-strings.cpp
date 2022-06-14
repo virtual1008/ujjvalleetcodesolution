14ff58e105e6eed33973362cb13df7b7909cbe78
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2022-06-14 09:41:24 +0530

    Time: 57 ms (18.66%), Space: 12.3 MB (17.03%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -12,8 +12,13 @@ public:
     int minDistance(string s1, string s2) {
         int n=s1.length();
         int m=s2.length();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        int c=rec(s1,s2,n-1,m-1,dp);
-        return n+m-2*c;
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1[i-1]==s2[j-1]) dp[i][j]=1+dp[i-1][j-1];
+                else dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+            }
+        }
+        return n+m-2*dp[n][m];
     }
 };
