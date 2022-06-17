92cc6b942e7fad04ec69cd1b636113f264c6141a
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2022-06-17 17:05:37 +0530

    Time: 62 ms (5.10%), Space: 29 MB (5.10%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.cpp
+++ 968-binary-tree-cameras/968-binary-tree-cameras.cpp
@@ -1,22 +1,45 @@
-class Solution 
-{
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
 public:
-    int res = 0;
-    
-    int minCameraCover(TreeNode* root) 
-    {
-        return (dfs(root) < 1 ? 1 : 0) + res;
-    }
-
-    int dfs(TreeNode* root) 
-    {
-        if (!root) return 2;
-        int left = dfs(root->left), right = dfs(root->right);
-        if (left == 0 || right == 0) 
-        {
-            res++;
-            return 1;
+    map<pair<TreeNode*,pair<bool,bool>>,int> dp;
+    int rec(TreeNode* root,bool cam,bool parcam){
+        if(root==NULL){
+            if(cam) return 1e6;
+            else return 0;
         }
-        return left == 1 || right == 1 ? 2 : 0;
+        if(root->left==NULL && root->right==NULL){
+            if(cam) return 1;
+            else{
+                if(parcam) return 0;
+                else 1e9;
+            }
+        }
+        if(dp.find({root,{cam,parcam}})!=dp.end()) return dp[{root,{cam,parcam}}];
+        if(cam){
+            return dp[{root,{cam,parcam}}]=1+min(rec(root->left,0,1),rec(root->left,1,1))+
+                min(rec(root->right,0,1),rec(root->right,1,1));
+        }else{
+            if(parcam){
+                return dp[{root,{cam,parcam}}]=min(rec(root->left,0,0),rec(root->left,1,0))+
+                    min(rec(root->right,0,0),rec(root->right,1,0));
+            }else{
+                int op1=rec(root->left,1,0)+min(rec(root->right,0,0),rec(root->right,1,0));
+                int op2=rec(root->right,1,0)+min(rec(root->left,0,0),rec(root->left,1,0));
+                 return dp[{root,{cam,parcam}}]=min(op1,op2);
+            }
+        }
+    }
+    int minCameraCover(TreeNode* root) {
+        return min(rec(root,0,0),rec(root,1,0));
     }
 };
