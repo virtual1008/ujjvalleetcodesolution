5149b8fe664b9e221c3a62a85565a2ba007f6d8f
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2022-06-25 10:04:10 +0530

    Time: 12 ms (100.00%), Space: 27.1 MB (12.14%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.cpp
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     bool checkPossibility(vector<int>& nums) {
-    vector<int> vec;
-    vec.push_back(nums[0]);
-    for(int i=1;i<nums.size();i++){
-        if(nums[i]>=vec[vec.size()-1]) vec.push_back(nums[i]);
-        else{
-            int l=0,r=vec.size()-1;
-            while(l<=r){
-                int mid=l+(r-l)/2;
-                if(vec[mid]>nums[i]){
-                    r=mid-1;
-                }else l=mid+1;
+        int cnt = 0;             
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] < nums[i-1]){
+                if(++cnt > 1) return false;
+                if(i == 1 || nums[i-2] <= nums[i]) nums[i-1] = nums[i];    
+                else nums[i] = nums[i-1];
             }
-            vec[l]=nums[i];
         }
-    }
-    return vec.size()+1>=nums.size();
-        
-    }
+        return true;
+    } 
 };
