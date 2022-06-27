32cc1c1eb7ec365298f9a4938f432d29ed116572
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2022-06-27 12:31:04 +0530

    Time: 245 ms (30.02%), Space: 6.6 MB (95.77%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
@@ -13,17 +13,21 @@ public:
         string t=s;
         reverse(t.begin(),t.end());
         int n=s.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+       // vector<vector<int>> dp(n+1,vector<int>(n+1,0));
         //return rec(n-1,n-1,s,t,dp);
+        vector<int> curr(n+1,0),temp(n+1,0);
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
                 if(s[i-1]==t[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
+                    curr[j]=1+temp[j-1];
+                    //dp[i][j]=1+dp[i-1][j-1];
                 }else{
-                    dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+                    curr[j]=max(curr[j-1],temp[j]);
+                    //dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
                 }
             }
+            temp=curr;
         }
-        return dp[n][n];
+        return curr[n];
     }
 };
