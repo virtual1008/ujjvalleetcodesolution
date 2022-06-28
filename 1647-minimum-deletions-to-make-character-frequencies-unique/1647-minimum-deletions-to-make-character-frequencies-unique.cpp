7153b4d640ebbf0797e10bc6bcaeb8b94480fcb7
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2022-06-28 09:31:01 +0530

    Time: 211 ms (9.16%), Space: 17.6 MB (21.80%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -9,14 +9,18 @@ public:
         int ans=0;
         int pre=v[0];
         for(int i=1;i<v.size();i++){
-            if(pre==v[i] && v[i]!=0){
-                ans++;
-                v[i]--;
-            }else if(v[i]>pre && v[i]!=0){
+            if(pre<=v[i]){
                 int d=v[i]-pre;
                 d++;
-                v[i]-d>=0?ans+=d:ans+=v[i];
-                v[i]-d>=0?v[i]-=d:v[i]=0;
+                if(v[i]-d>=0){
+                    v[i]-=d;
+                    ans+=d;
+                }else{
+                    //v[i]=0;
+                    ans+=v[i];
+                    v[i]=0;
+                }
+                
             }
             pre=v[i];
         }
