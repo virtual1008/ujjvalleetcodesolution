d94fe9c7250dc294b347a7d8395acd0d1e4d767b
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2022-08-01 18:21:01 +0530

    Time: 215 ms (30.82%), Space: 5.9 MB (81.19%) - LeetHub

--- 526-beautiful-arrangement/526-beautiful-arrangement.cpp
+++ 526-beautiful-arrangement/526-beautiful-arrangement.cpp
@@ -9,16 +9,13 @@ public:
     }
     void rec(int i,vector<int> &arr){
         if(i==arr.size()) cnt++;
+        
             for(int j=i;j<arr.size();j++){
-                swap(arr,i,j);
-                if(arr[i]%(i+1)==0 || (i+1)%arr[i]==0)rec(i+1,arr);
-                swap(arr,i,j);
+                swap(arr[i],arr[j]);
+                if(arr[i]%(i+1)==0 || (i+1)%arr[i]==0)
+                rec(i+1,arr);
+                swap(arr[i],arr[j]);
             }
         
-    }
-    void swap(vector<int> &arr,int i,int j){
-        int temp=arr[i];
-        arr[i]=arr[j];
-        arr[j]=temp;
     }
 };
