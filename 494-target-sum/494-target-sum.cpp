182956970e6966b50ae01436ec6644f01dbaf54e
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2022-08-02 10:08:04 +0530

    Time: 419 ms (30.10%), Space: 122.3 MB (5.00%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
-    int rec(int i,int sum,int temp,vector<int>& nums){
-        if(i<0){
-            if(sum==temp) return 1;
-            else return 0;
+    int rec(int i,int sum,vector<int> nums,vector<vector<int>> &dp){
+        if(i>=nums.size()) {
+            if(sum==0) return 1;
+            return 0;
         }
-        int take=0,ntake=0;
-        if(temp+nums[i]<=sum) take=rec(i-1,sum,temp+nums[i],nums);
-        ntake=rec(i-1,sum,temp,nums);
-        return take+ntake;
+        if(dp[i][sum]!=-1) return dp[i][sum];
+       // if(sum==0) return 1;
+        int take=0;
+        if(sum>=nums[i])take=rec(i+1,sum-nums[i],nums,dp);
+        int ntake=rec(i+1,sum,nums,dp);
+        return dp[i][sum]=take+ntake;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int sum=0;
         for(auto it:nums) sum+=it;
-        sum+=target;
-        if(sum%2!=0) return 0;
-        sum/=2;
-        return rec(nums.size()-1,sum,0,nums);
+        //sort(nums.begin(),nums.end());
+        int d=sum+target;
+        //if(d<0) return 0;
+        if(d%2!=0 ||d<0) return 0;
+        d/=2;
+        vector<vector<int>> dp(nums.size()+1,vector<int>(d+1,-1));
+        return rec(0,d,nums,dp);
+        
     }
 };
