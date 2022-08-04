7c866351f342eebe929b6e226692a508eec256ca
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2022-08-04 11:16:11 +0530

    Time: 5 ms (12.40%), Space: 5.8 MB (62.81%) - LeetHub

--- 858-mirror-reflection/858-mirror-reflection.cpp
+++ 858-mirror-reflection/858-mirror-reflection.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
     int mirrorReflection(int p, int q) {
-        while (p % 2 == 0 && q % 2 == 0){ 
-            p/=2;
-            q/=2;
-        }
+        int gcd=__gcd(p,q);
+        p/=gcd,q/=gcd;
         return 1 - p % 2 + q % 2;
     }
 };
