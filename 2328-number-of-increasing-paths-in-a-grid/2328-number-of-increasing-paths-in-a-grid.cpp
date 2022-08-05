a028e27057351d3c4d27e3cee0384d402636d51e
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2022-08-05 17:17:38 +0530

    Time: 523 ms (30.21%), Space: 43.6 MB (73.70%) - LeetHub

--- 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.cpp
+++ 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.cpp
@@ -1,33 +1,30 @@
-static int MOD = 1e9+7;
-static vector<vector<int>> dir = {{0,1},{0,-1},{1,0},{-1,0}};
 class Solution {
-    vector<vector<long long>> dp;
-    int n,m;
-    long long dfs(vector<vector<int>>& grid,int i,int j)
-    {
-        long long res=1;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        for(int k=0;k<4;k++)
-        {
-            if(i+dir[k][0]>=0 && i+dir[k][0]<n && j+dir[k][1]>=0 && j+dir[k][1]<m && grid[i+dir[k][0]][j+dir[k][1]]>grid[i][j])
-                res=(res+dfs(grid,i+dir[k][0],j+dir[k][1]))%MOD;
-        }
-        return dp[i][j]=res%MOD;
-    }
 public:
-    int countPaths(vector<vector<int>>& grid) {
-        n=grid.size();
-        m=grid[0].size();
-        dp=vector<vector<long long>>(n,vector<long long>(m,-1));
-        long long res=0;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                res=(res+dfs(grid,i,j))%MOD;
+    int n,m;
+    int mod=1e9+7;
+    vector<vector<int>> dp;
+    vector<int> path={1,0,-1,0,1}; 
+    int rec(int i,int j,vector<vector<int>>& grid){
+        if(dp[i][j]!=-1) return dp[i][j];
+        int val=1;
+        for(int k=0;k<4;k++){
+            if(i+path[k]>=0 && i+path[k]<n && j+(path[k+1])>=0 && j+path[k+1]<m && grid[i+path[k]][j+path[k+1]]>grid[i][j]){
+               val=(val%mod+rec(i+path[k],j+path[k+1],grid)%mod)%mod;
             }
         }
-        return res%MOD;
+        return dp[i][j]=val%mod;
+    }
+    int countPaths(vector<vector<int>>& grid) {
+          n=grid.size();
+          m=grid[0].size();
+          dp= vector<vector<int>>(n,vector<int>(m,-1));
+          int ans=0;
+          for(int i=0;i<n;i++){
+              for(int j=0;j<m;j++){
+                  ans=(ans%mod+rec(i,j,grid)%mod);
+              }
+          }
+         return ans%mod;
+         
     }
 };
