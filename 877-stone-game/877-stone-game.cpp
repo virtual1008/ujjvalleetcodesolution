7aa32170c6f12590dda22d26ac488e826d3e13b3
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2022-08-15 10:15:08 +0530

    Time: 2926 ms (5.04%), Space: 696.9 MB (5.04%) - LeetHub

--- 877-stone-game/877-stone-game.cpp
+++ 877-stone-game/877-stone-game.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    int dp[502][502][2];
-    int rec(int l,int r,int k,vector<int> prices){
+    vector<vector<vector<int>>> dp;
+    int rec(int l,int r,int k,vector<int> p){
         if(l>r) return 0;
         if(dp[l][r][k]!=-1) return dp[l][r][k];
         if(k){
-            return dp[l][r][k]=max(prices[l]+rec(l+1,r,1-k,prices),prices[r]+rec(l,r-1,1-k,prices));
+            return dp[l][r][k]=max(p[l]+rec(l+1,r,1-k,p),p[r]+rec(l,r-1,1-k,p));
         }else{
-            return dp[l][r][k]=min(rec(l+1,r,1-k,prices),rec(l,r-1,1-k,prices));
+            return dp[l][r][k]=max(-p[l]+rec(l+1,r,1-k,p),-p[r]+rec(l,r-1,1-k,p));
         }
     }
-    bool stoneGame(vector<int>& piles) {
-        int sum=0;
-        for(auto it:piles) sum+=it;
-        memset(dp,-1,sizeof(dp));
-        return 2*(sum-rec(0,piles.size()-1,1,piles))<sum; 
+    bool stoneGame(vector<int>& p) {
+        dp= vector<vector<vector<int>>>(p.size(), vector<vector<int>>(p.size(),vector<int>(2,-1)));
+        int c=rec(0,p.size()-1,1,p);
+        cout<<c;
+        cout<<"\n";
+        return c>0?"Alice":"Bob";
     }
-};
+}; 
