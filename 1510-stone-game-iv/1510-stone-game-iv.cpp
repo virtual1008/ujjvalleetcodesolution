bfac2b804fb55cd7fcfd3252f0476d622abfd0e8
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2022-08-15 16:17:04 +0530

    Time: 970 ms (5.01%), Space: 35 MB (6.67%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> dp;
-    int rec(int n,int k){
+    bool rec(int n,int k){
         if(n==0) return 1-k;
         int ans=1-k;
         if(dp[n][k]!=-1) return dp[n][k];
         for(int i=1;i*i<=n;i++){
-            int val=rec(n-i*i,1-k);
+            int cv=rec(n-i*i,1-k);
             if(k){
-                if(val==1){
+                if(cv==1){
                     ans=1;
                     break;
                 }
             }else{
-                if(val==0){
+                if(cv==0){
                     ans=0;
                     break;
                 }
@@ -23,6 +23,6 @@ public:
     }
     bool winnerSquareGame(int n) {
         dp=vector<vector<int>>(n+1,vector<int>(2,-1));
-      return rec(n,1);   
+        return rec(n,1);
     }
 };
