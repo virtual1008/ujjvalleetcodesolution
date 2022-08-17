cc3fff031fc7b073206fbadbcded42c0927b38bb
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2022-08-17 11:54:04 +0530

    Time: 8 ms (61.92%), Space: 19.1 MB (13.49%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
+   vector<vector<int>> dp;
     int rec(int i,int j,string s,string p){
-        if(j<0)
-       {
-           if(i<0)
-               return true;
-           else
-               return false;
-       }
+        if(i<0 && j<0 ) return true;
+        if(j<0) return false;
         if(i<0){
             while(j>=0){
                 if(p[j]=='*') j--;
-                else return false;
+                else return  false;
                 j--;
             }
             return true;
         }
+        if(dp[i][j]!=-1) return dp[i][j];
         if(s[i]==p[j] || p[j]=='.') return rec(i-1,j-1,s,p);
         else if(p[j]=='*'){
-            int result=rec(i,j-2,s,p);
-            if(s[i]==p[j-1] || p[j-1]=='.') result=result || rec(i-1,j,s,p);
-            return result;
-        }else
-        return false;
+            bool result=rec(i,j-2,s,p);
+            if(s[i]==p[j-1] || p[j-1]=='.') result = result || rec(i-1,j,s,p);
+            return dp[i][j]=result;
+        }
+        return dp[i][j]=false;
     }
     bool isMatch(string s, string p) {
+        int n=s.size();
+        int m=p.size();
+        dp=vector<vector<int>>(n+1,vector<int>(m+1,-1));
         return rec(s.size()-1,p.size()-1,s,p);
     }
 };
