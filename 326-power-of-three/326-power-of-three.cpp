1122fb6c68f90ac72462f1e53b9451638f7b72ab
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2022-08-24 11:19:05 +0530

    Time: 28 ms (68.40%), Space: 6.2 MB (7.72%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n==0) return false;
-        while(n%3==0){
-            n/=3;
-        }
-        return n==1?true:false;
+        if(n<=0) return false;
+        double x=log10(n)/log10(3);
+        return x==(int )x;
     }
 };
