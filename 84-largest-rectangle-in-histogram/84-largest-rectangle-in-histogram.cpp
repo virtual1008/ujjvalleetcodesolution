1d82777ce92f04b5db54bdab0aed9f843cdbc5e2
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2022-08-25 11:03:50 +0530

    Time: 246 ms (70.70%), Space: 85.2 MB (29.29%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
+ 
     int largestRectangleArea(vector<int>& v) {
-        int n = v.size();
-        vector<int> pre(n, -1), suf(n, n);
-        stack<int> lft, rgt;
-        
-        for(int i=0, j=n-1; i<n; j--, i++){
-            while(!lft.empty() and v[lft.top()] >= v[i]) lft.pop();
-            if(!lft.empty()) pre[i] = lft.top();
-            lft.push(i);
-            
-            while(!rgt.empty() and v[rgt.top()] >= v[j]) rgt.pop();
-            if(!rgt.empty()) suf[j] = rgt.top();
-            rgt.push(j);
+        //vector<int> ans=nsn(v);
+        stack<int> st,st1;
+        vector<int> ans(v.size(),v.size()-1);
+        vector<int> ans1(v.size(),0);
+        for(int i=0,j=v.size()-1;i<v.size(),j>=0;i++,j--){
+            while(!st.empty() && v[i]<v[st.top()]){
+                ans[st.top()]=i-1;
+                st.pop();
+            }
+            st.push(i);
+            while(!st1.empty() && v[j]<v[st1.top()]){
+                ans1[st1.top()]=j+1;
+                st1.pop();
+            }
+            st1.push(j);
         }
-        
-        int ans = 0;
-        for(int i=0; i<n; i++){
-            ans = max(ans, (suf[i] - pre[i] - 1)*v[i]);
+        //for(auto it:ans) cout<<it<<" ";
+        //cout<<"\n";
+        //vector<int> ans1=psn(v);
+       // for(auto it:ans1) cout<<it<<" ";
+        int val=0;
+        for(int i=0;i<v.size();i++){
+            val=max(val,(ans[i]-ans1[i]+1)*v[i]);
         }
-        return ans;
+        return val;
     }
 };
