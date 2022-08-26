46debb046a6b2aa3226c176d54da73ba54dcea38
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2022-08-26 11:13:51 +0530

    Time: 7 ms (42.94%), Space: 6 MB (68.93%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
     bool reorderedPowerOf2(int n) {
-        map<int,int> mp;
-        while(n>0){
-            mp[n%10]++;
-            n/=10;
-        }
-        map<int,map<int,int>> cnt;
+        string str=to_string(n);
+        sort(str.begin(),str.end());
+        //vector<string> ans;
         for(int i=0;i<31;i++){
             int val=1<<i;
-            int k=val;
-            map<int,int> np;
-            while(k>0){
-               np[k%10]++;
-               k/=10;
-            }
-            cnt[val]=np;
-            
+            string k=to_string(val);
+            sort(k.begin(),k.end());
+            if(str==k) return true;
         }
-        bool flag=false;
-        for(auto it:cnt){
-            if(it.second==mp) flag=true;
-        }
-        return flag;
+        return false;
+        
     }
 };
