1a5af64615b1edfd9e6b209d8bd559e3dd1ba424
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2022-08-28 12:15:22 +0530

    Time: 1836 ms (5.01%), Space: 8.3 MB (6.18%) - LeetHub

---
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int rec(int i,int m,vector<int> &nums){
+        if(i==nums.size()) return -1;
+        int ans=INT_MAX;
+        int sum=0;
+        if(dp[i][m]!=-1) return dp[i][m];
+        for(int j=i;j<nums.size();j++){
+            sum+=nums[j];
+            if(nums.size()-(j+1)<m-1) break;
+            ans=min(ans,max(sum,rec(j+1,m-1,nums)));
+        }
+        return dp[i][m]=ans;
+    }
+    int splitArray(vector<int>& nums, int m) {
+        dp=vector<vector<int>>(nums.size()+1,vector<int>(m+1,-1));
+        return rec(0,m,nums);
+    }
+};
