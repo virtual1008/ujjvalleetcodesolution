5bf1530afb8041cf84bac983954dddb0b3e4b781
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2022-08-28 11:06:48 +0530

    Time: 41 ms (6.35%), Space: 11.6 MB (5.01%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -3,12 +3,15 @@ public:
     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
          int n=mat.size();
          int m=mat[0].size();
+         map<pair<int,int>,bool> mp;
          for(int i=0;i<n;i++){
              for(int j=0;j<m;j++){
                  vector<int> temp;
                  priority_queue<int,vector<int>,greater<int>> pq;
                  int a=i,b=j;
+                 if(mp[{a,b}]) continue;
                  while(a<n && b<m){
+                     mp[{a,b}]=true;
                      pq.push(mat[a][b]);
                      a++;
                      b++;
