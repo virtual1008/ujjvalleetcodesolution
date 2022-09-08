e631221cc9bc7e3bb02408c41a21da592405cdca
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2022-09-08 15:00:44 +0530

    Time: 2612 ms (5.78%), Space: 285.2 MB (15.21%) - LeetHub

---
+++ 446-arithmetic-slices-ii-subsequence/446-arithmetic-slices-ii-subsequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+       int ans=0;
+       vector<unordered_map<long long ,long long>> mp(nums.size());
+        for(int i=1;i<nums.size();i++){
+            for(int j=0;j<i;j++){
+                long long d=(long long)nums[i];
+                d-=(long long)nums[j];
+                if(mp[j][d]!=0) ans+=mp[j][d];
+                mp[i][d]=mp[j][d]+mp[i][d]+1;
+            }
+        }
+        return ans;
+    }
+};
