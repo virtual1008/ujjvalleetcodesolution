22e25526f941523dc44ee72c4cbb4c5e5204f0b8
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2022-09-08 11:57:02 +0530

    Time: 14 ms (98.32%), Space: 16.9 MB (86.43%) - LeetHub

---
+++ 136-single-number/136-single-number.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int temp=nums[0];
+        for(int i=1;i<nums.size();i++) temp^=nums[i];
+        return temp;
+    }
+};
