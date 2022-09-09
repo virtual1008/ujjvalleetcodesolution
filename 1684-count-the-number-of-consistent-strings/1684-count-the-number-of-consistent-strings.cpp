3d500b384eb933e2ed7a45404141dbd6a4b57ab5
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2022-09-09 12:31:46 +0530

    Time: 163 ms (10.44%), Space: 30.1 MB (65.27%) - LeetHub

--- 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cpp
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int countConsistentStrings(string allowed, vector<string>& words) {
-        vector<int> v(26,0);
-        for(auto it:allowed){
-            v[it-'a']=1;
-        }
         int ans=0;
+        int cnt=0;
+        for(auto it:allowed){
+            cnt|=(1<<(it-'a'));
+        }
         for(auto it:words){
-            vector<int> temp(26,0);
-            for(auto ch:it){
-                temp[ch-'a']=1;
-            }
             bool flag=true;
-            for(int i=0;i<26;i++){
-                if(temp[i]==1 && v[i]==0){
+            for(auto ele:it){
+                if(!(cnt & (1<<(ele-'a')))){
                     flag=false;
                     break;
                 }
