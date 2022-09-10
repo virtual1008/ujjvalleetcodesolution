5d0a501deea0b7530a3212eeb323bed738048cd6
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2022-09-10 09:59:57 +0530

    Time: 103 ms (10.66%), Space: 21.3 MB (6.51%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
-    int rec(int i,int j,int k,vector<int>& prices, vector<vector<vector<int>>> &dp){
-        if(i>=prices.size()) return 0;
+    vector<vector<vector<int>>> dp;
+    int  rec(int i,int k,bool buy,vector<int> &prices){
         if(k<=0) return 0;
-        if(dp[i][j][k]!=-1) return dp[i][j][k];
-        if(j){
-            return dp[i][j][k]=max(-prices[i]+rec(i+1,0,k,prices,dp),rec(i+1,1,k,prices,dp));
+        if(i>=prices.size()) return 0;
+        if(dp[i][k][buy]!=-1) return dp[i][k][buy];
+        if(buy){
+            return dp[i][k][buy]=max(-prices[i]+rec(i+1,k,1-buy,prices),rec(i+1,k,buy,prices));
         }else{
-            return dp[i][j][k]=max(prices[i]+rec(i+1,1,k-1,prices,dp),rec(i+1,0,k,prices,dp));
+            return dp[i][k][buy]=max(prices[i]+rec(i+1,k-1,1-buy,prices),rec(i+1,k,buy,prices));
         }
     }
-    int maxProfit(int t, vector<int>& prices) {
-        int n=prices.size();
-        //vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(t+1,0)));
-        vector<vector<int>> cur(2,vector<int>(t+1,0)),pre(2,vector<int>(t+1,0));
-         for(int i=n-1;i>=0;i--){
-             for(int j=0;j<=1;j++){
-                 for(int k=1;k<=t;k++){
-                     if(j){
-                         cur[j][k]=max(-prices[i]+pre[0][k],pre[1][k]);
-                     }else{
-                         cur[j][k]=max(prices[i]+pre[1][k-1],pre[0][k]);
-                     }
-                 }
-             }
-             pre=cur;
-         }
-        return cur[1][t];
+    int maxProfit(int k, vector<int>& prices) {
+        dp=vector<vector<vector<int>>>(prices.size(),vector<vector<int>>(k+1,vector<int>(2,-1)));
+        return rec(0,k,true,prices);
     }
 };
