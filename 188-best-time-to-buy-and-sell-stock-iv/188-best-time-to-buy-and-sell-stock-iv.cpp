01273c76b2fc4c7f565ed13467f7753482239400
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2022-09-10 10:30:33 +0530

    Time: 19 ms (54.05%), Space: 10.8 MB (84.96%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
-    int maxProfit(int t, vector<int>& prices) {
+   
+     int maxProfit(int t, vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(t+1,vector<int>(2,0)));
-        for(int i=n-1;i>=0;i--){
-            for(int j=0;j<=1;j++){
-                for(int k=1;k<=t;k++){
-                    if(j){
-                        dp[i][k][j]=max(-prices[i]+dp[i+1][k][1-j],dp[i+1][k][j]);
-                    }else{
-                        dp[i][k][j]=max(prices[i]+dp[i+1][k-1][1-j],dp[i+1][k][j]);
-                    }
-                }
-            }
-        }
-        return dp[0][t][1];
+        //vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(t+1,0)));
+        vector<vector<int>> cur(2,vector<int>(t+1,0)),pre(2,vector<int>(t+1,0));
+         for(int i=n-1;i>=0;i--){
+             for(int j=0;j<=1;j++){
+                 for(int k=1;k<=t;k++){
+                     if(j){
+                         cur[j][k]=max(-prices[i]+pre[0][k],pre[1][k]);
+                     }else{
+                         cur[j][k]=max(prices[i]+pre[1][k-1],pre[0][k]);
+                     }
+                 }
+             }
+             pre=cur;
+         }
+        return cur[1][t];
     }
 };
