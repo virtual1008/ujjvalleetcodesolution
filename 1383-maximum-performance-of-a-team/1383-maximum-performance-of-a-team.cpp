618447dc6c7323747db668e3ad9200e328ff484a
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2022-09-11 13:16:36 +0530

    Time: 235 ms (14.26%), Space: 36.9 MB (56.16%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
@@ -19,7 +19,33 @@ public:
         pq.push(currSpeed);
         totSpeed += currSpeed; 
         ans = max(ans, totSpeed * currEff); 
-      }
+      }class Solution {
+public:
+    int maxPerformance(int n, vector<int>&a, vector<int>& b, int k) {
+        vector<pair<int,int>> v;
+        for(int i=0;i<n;i++){
+            v.push_back({b[i],a[i]});
+            
+            
+        }
+        sort(v.begin(),v.end(),greater<pair<int,int>>());
+        long long ans=0;
+        long long sum=0;
+        int mod=1e9+7;
+        priority_queue<int,vector<int>,greater<int>> pq;
+        for(int i=0;i<n;i++){
+            sum+=(v[i].second);
+            if(pq.size()==k){
+                sum-=(pq.top());
+                pq.pop();
+            };
+            ans=max(ans,((sum)*(v[i].first)));
+            pq.push(v[i].second);
+            //cout<<sum*v[i].first<<" ";
+        }
+        return ans;
+    }
+};
       return ans % mod;
     }
 };
