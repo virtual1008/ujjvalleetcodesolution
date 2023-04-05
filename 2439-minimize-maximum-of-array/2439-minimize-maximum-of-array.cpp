46e884a88e2a2f2167d8824f846f72512f2db4df
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2023-04-05 16:16:33 +0530

    Time: 150 ms (58.19%), Space: 71.2 MB (90.47%) - LeetHub

---
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimizeArrayValue(vector<int>& nums) {
+        long long sum=0;
+        long long ans=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            ans=max(ans,(sum+i)/(i+1));
+        }
+        return ans;
+        
+    }
+};
