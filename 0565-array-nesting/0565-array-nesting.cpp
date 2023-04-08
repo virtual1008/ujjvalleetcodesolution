aa8985540090d7676b0568a0a1ba3b7d331c8629
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2023-04-08 12:20:58 +0530

    Time: 175 ms (29.61%), Space: 102.9 MB (23.53%) - LeetHub

---
+++ 0565-array-nesting/0565-array-nesting.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void rec(vector<bool> & vis,int i,vector<int> &dist,int & len,vector<int> & nums ){
+        if(vis[i]) {
+           dist[i]=max(len,dist[i]);
+            return;
+        }
+        vis[i]=1;
+        len+=1;
+        rec(vis,nums[i],dist,len,nums);
+        dist[i]=len;
+    }
+    int arrayNesting(vector<int>& nums) {
+        vector<bool> vis(nums.size(),false);
+        vector<int> dist(nums.size(),1);
+        for(int i=0;i<nums.size();i++){
+            int len=0;
+                rec(vis,i,dist,len,nums);
+        }
+        int maxi=0;
+        for(auto it:dist){
+            maxi=max(maxi,it);
+        }
+        return maxi;
+    }
+};
