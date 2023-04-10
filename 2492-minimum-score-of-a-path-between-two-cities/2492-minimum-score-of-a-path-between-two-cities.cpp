fcef2c95d0dfb76020363fddd3f32be3db75efbd
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2023-04-10 11:42:09 +0530

    Time: 545 ms (39.49%), Space: 126.1 MB (73.29%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,41 +1,43 @@
 class Solution {
 public:
-    // we know that there is atleast one path from 1 to n so our task is to find all the nodes which is the part of the 1 to n component
-    void dfs(int node ,vector<int> & vis,vector<int> adj[]){
-        vis[node]=1;
-        for(auto it:adj[node]){
-            if(vis[it]==0){
-                dfs(it,vis,adj);
+    // by union find
+    vector<int> parent,rank;
+    int find(int a){
+        if(a==parent[a]) return a;
+        return parent[a]=find(parent[a]);
+    }
+    void dsu(int a,int b){
+        a=find(a);
+        b=find(b);
+        if(a==b) return;
+        else{
+            if(rank[a]>=rank[b]){
+                parent[b]=a;
+            }else{
+                parent[a]=b;
             }
+            if(rank[a]==rank[b]) rank[a]++;
         }
     }
     int minScore(int n, vector<vector<int>>& roads) {
-        vector<int> exist(n+1,0);
-        vector<int> adj[n+1];
-        map<pair<int,int>,int> mp;
+        parent=vector<int>(n+1,0);
+        rank=vector<int>(n+1,0);
+        for(int i=1;i<=n;i++) parent[i]=i;
         for(auto it:roads){
             int a=it[0];
             int b=it[1];
-            int c=it[2];
-            adj[a].push_back(b);
-            adj[b].push_back(a);
-            if(a>b) swap(a,b);
-            if(mp[{a,b}]){
-                mp[{a,b}]=min(mp[{a,b}],c);
-            }else{
-                mp[{a,b}]=c;
-            }
+            dsu(a,b);
         }
-        dfs(1,exist,adj);
         int ans=INT_MAX;
-        for(int i=1;i<=n;i++){
-            for(auto it:adj[i]){
-                int a=i;
-                int b=it;
-                if(exist[a] && exist[b]){
-                    if(a>b) swap(a,b);
-                ans=min(ans,mp[{a,b}]);
-                }
+        for(auto it:roads){
+            int a=it[0];
+            int b=it[1];
+            int d=it[2];
+            int p=find(1);
+            a=find(a);
+            b=find(b);
+            if(a==p && b==p){
+                ans=min(ans,d);
             }
         }
         return ans;
