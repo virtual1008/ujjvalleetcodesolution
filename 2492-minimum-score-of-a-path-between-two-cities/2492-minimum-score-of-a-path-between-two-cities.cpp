1d0fb6b966c67878cf7f01ac9d21693fee811c8b
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2023-04-10 11:31:33 +0530

    Time: 832 ms (8.39%), Space: 151.7 MB (27.22%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,19 +1,43 @@
 class Solution {
-    void dfs(int node, vector<pair<int, int>> adj[], vector<int> &vis, int &ans) {
-        vis[node] = 1;
-        for(auto ad : adj[node]) {
-            ans = min(ans, ad.second);
-            if(!vis[ad.first]) dfs(ad.first, adj, vis, ans);
+public:
+    // we know that there is atleast one path from 1 to n so our task is to find all the nodes which is the part of the 1 to n component
+    void dfs(int node ,vector<int> & vis,vector<int> adj[]){
+        vis[node]=1;
+        for(auto it:adj[node]){
+            if(vis[it]==0){
+                dfs(it,vis,adj);
+            }
         }
     }
-public:
     int minScore(int n, vector<vector<int>>& roads) {
-        vector<pair<int, int>> adj[n + 1];
-        for(auto e : roads) adj[e[0]].push_back({e[1], e[2]}), adj[e[1]].push_back({e[0], e[2]});
-        
-        int ans = 1e9;
-        vector<int> vis(n + 1);
-        dfs(n, adj, vis, ans);
+        vector<int> exist(n+1,0);
+        vector<int> adj[n+1];
+        map<pair<int,int>,int> mp;
+        for(auto it:roads){
+            int a=it[0];
+            int b=it[1];
+            int c=it[2];
+            adj[a].push_back(b);
+            adj[b].push_back(a);
+            if(a>b) swap(a,b);
+            if(mp[{a,b}]){
+                mp[{a,b}]=min(mp[{a,b}],c);
+            }else{
+                mp[{a,b}]=c;
+            }
+        }
+        dfs(1,exist,adj);
+        int ans=INT_MAX;
+        for(int i=1;i<=n;i++){
+            for(auto it:adj[i]){
+                int a=i;
+                int b=it;
+                if(exist[a] && exist[b]){
+                    if(a>b) swap(a,b);
+                ans=min(ans,mp[{a,b}]);
+                }
+            }
+        }
         return ans;
     }
 };
