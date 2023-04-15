0ec7aea1af4ef1b1b0497a3df70499c96cf8f5c1
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2023-04-15 17:45:02 +0530

    Time: 337 ms (41.88%), Space: 18 MB (80.63%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
@@ -9,17 +9,16 @@ public:
         }
         if(dp[ind][k]!=-1) return dp[ind][k];
         int ans=-1e6;
+        int temp=0;
         for(int i=0;i<min(k,(int)(piles[ind].size()));i++){
-             ans=max(ans,piles[ind][i]+rec(ind+1,k-(i+1),piles));
+            temp+=piles[ind][i];
+             ans=max(ans,temp+rec(ind+1,k-(i+1),piles));
         }
         return dp[ind][k]=max(ans,rec(ind+1,k,piles));
     }
     int maxValueOfCoins(vector<vector<int>>& piles, int k) {
         int n=piles.size();
         dp=vector<vector<int>>(n+1,vector<int>(k+1,-1));
-        for (int i = 0; i < n; i++)
-            for (int j = 1; j < piles[i].size(); j++)
-                piles[i][j] += piles[i][j - 1];
         
         return rec(0,k,piles);
         
