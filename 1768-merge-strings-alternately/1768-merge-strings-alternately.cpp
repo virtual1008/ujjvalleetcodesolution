4fb9f358ebcee79ea6e088ea527d7962fb21b976
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2023-04-18 10:48:43 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (19.57%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string ans="";
+        int i=0,j=0,n=word1.size(),m=word2.size();
+        //if(n<m) swap(word1,word2);
+        while(i<n && j<m){
+            ans+=word1[i];
+            ans+=word2[j];
+            i++;
+            j++;
+        }
+        while(i<n){
+            ans+=word1[i];
+            i++;
+        }
+        while(j<m){
+            ans+=word2[j];
+            j++;
+        }
+        return ans;
+    }
+};
