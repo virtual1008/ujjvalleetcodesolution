7d00a06b117a13958b1965d067dea98c2cd3b89c
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2023-05-29 09:28:29 +0530

    Time: 1600 ms (5.19%), Space: 463.6 MB (5.04%) - LeetHub

---
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maxProduct(vector<string>& words) {
+        unordered_map<string,int> mp;
+        for(auto it:words){
+            int c=0;
+            if(mp[it]!=0) continue;
+            for(int i=0;i<it.size();i++){
+                c|=(1<<(it[i]-'a'));
+            }
+            mp[it]=c;
+            
+        }
+        int ans=0;
+        for(auto it:mp){
+            int c1=it.second;
+            string s1=it.first;
+            for(auto e:mp){
+                string s2=e.first;
+                int c2=e.second;
+                if(c1&c2) continue;
+                int k=s1.size();
+                k*=s2.size();
+                if(ans<k) ans=k;
+                //ans=max(ans,(s1.size())*(s2.size()));
+            }
+        }
+        return ans;
+    }
+};
