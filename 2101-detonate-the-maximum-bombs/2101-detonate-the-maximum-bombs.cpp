ca604d973d47fb48d3f9e79c4bbb947c600d2de8
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2023-06-02 10:00:51 +0530

    Time: 126 ms (71.92%), Space: 17.8 MB (52.59%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
@@ -1,38 +1,41 @@
 class Solution {
 public:
-    void dfs(int i, int &count, vector<bool> &visited, vector<vector<int>> &adj){
-        visited[i] = true;
-        count++;
-        for(auto it : adj[i])
-        {
-            if(!visited[it])
-            dfs(it, count, visited, adj);
+    void rec(int i,vector<int> adj[],vector<int> &vis,int &cnt){
+        vis[i]=true;
+        cnt++;
+        for(auto it:adj[i]){
+            if(!vis[it]){
+                rec(it,adj,vis,cnt);
+            }
         }
+        return ;
     }
     int maximumDetonation(vector<vector<int>>& bombs) {
-       int n = bombs.size();
-       vector<vector<int>> adj(n);
-       for(int i=0; i<n; i++){
-           long long r1 = bombs[i][2];
-           long long x1 = bombs[i][0], y1 = bombs[i][1];
-           for(int j = 0; j<n; j++){
-             if(j != i){
-                 long long x2 = bombs[j][0], y2 = bombs[j][1];
-                 long long dist = (x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2);
-                 if(dist <= r1*r1)
-                 adj[i].push_back(j);
-             }
-           }
-       }
-       vector<bool> visited(n, 0);
-       int ans = 0;
-       for(int i=0; i<n; i++)
-       {
-           int count = 0;
-           dfs(i, count, visited, adj);
-           ans = max(ans, count);
-           fill(visited.begin(), visited.end(), 0);
-       } 
-       return ans;
+        int n=bombs.size();
+        vector<int> adj[n];
+        for(int i=0;i<bombs.size();i++){
+            long long x1=bombs[i][0];
+            long long y1=bombs[i][1];
+            long long r1=bombs[i][2];
+            for(int j=0;j<bombs.size();j++){
+                if(i==j) continue;
+                long long x2=bombs[j][0];
+                long long y2=bombs[j][1];
+                long long r2=bombs[j][2];
+                long long dist=(x1-x2)*(x1-x2)+(y1-y2)*(y1-y2);
+                if(r1*r1>=dist){
+                    adj[i].push_back(j);
+                }
+            }
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            vector<int> vis(n,0);
+            int cnt=0;
+            rec(i,adj,vis,cnt);
+            ans=max(ans,cnt);
+        }
+        return ans;
+        
     }
 };
