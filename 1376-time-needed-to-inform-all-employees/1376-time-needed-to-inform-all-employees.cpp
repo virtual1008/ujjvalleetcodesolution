12c9fa33b3de32b848d91bd210ba432af0f5e8dd
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2023-06-03 10:44:54 +0530

    Time: 299 ms (82.02%), Space: 112.6 MB (87.70%) - LeetHub

---
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int rec(int node,int parent,vector<int> adj[],vector<int>& informTime,vector<int> & manage){
+        int ans=0;
+        for(auto it:adj[node]){
+            if(it==parent) continue;
+            ans=max(ans,rec(it,node,adj,informTime,manage));
+        }
+        return ans+informTime[node];
+    }
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+        vector<int> adj[n];
+        for(int i=0;i<manager.size();i++){
+            if(manager[i]==-1) continue;
+            adj[manager[i]].push_back(i);
+        }
+        return rec(headID,-1,adj,informTime,manager);
+    }
+};
