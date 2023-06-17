3cf6a760cb900c5a08c1cb501cec6d6986c78275
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2023-06-17 11:01:30 +0530

    Time: 19 ms (44.11%), Space: 15.9 MB (68.51%) - LeetHub

---
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        for(auto it:letters){
+            if(it>target) return it;
+        }
+        return letters[0];
+    }
+};
