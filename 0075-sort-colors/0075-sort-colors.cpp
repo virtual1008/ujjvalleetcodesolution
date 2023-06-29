c59788588bdf7d54687d522b5b1e0e52df30e0e9
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2023-06-29 22:15:14 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (8.31%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,10 +1,27 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        for(int i=0;i<nums.size();i++){
-            for(int j=0;j<i;j++){
-                if(nums[j]>nums[i]) swap(nums[j],nums[i]);
-            }
+        int z=0,o=0,t=0;
+        for(auto it:nums){
+            if(it==0) z++;
+            else if(it==1) o++;
+            else t++;
+        }
+        int i=0;
+        while(z>0){
+            nums[i]=0;
+            z--;
+            i++;
+        }
+        while(o>0){
+            nums[i]=1;
+            o--;
+            i++;
+        }
+        while(t>0){
+            nums[i]=2;
+            t--;
+            i++;
         }
     }
 };
