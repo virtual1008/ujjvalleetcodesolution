d91d470a05a89badabbfc2450e4d14acec58b37e
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2023-07-01 17:05:13 +0530

    Time: 37 ms (38.84%), Space: 13.7 MB (40.18%) - LeetHub

--- 1879-minimum-xor-sum-of-two-arrays/1879-minimum-xor-sum-of-two-arrays.cpp
+++ 1879-minimum-xor-sum-of-two-arrays/1879-minimum-xor-sum-of-two-arrays.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    int solve(int s,vector<int> &v1,vector<int> &v2,int mask,vector<int> &dp){
-        if(s==v2.size()){
-            return 0;
-        }
+    vector<int> dp;
+    int rec(int s, int  mask, vector<int> & nums1 ,vector<int> & nums2){
+        if(s>=nums2.size()) return 0;
         if(dp[mask]==INT_MAX){
-            for(int j = 0; j < v1.size(); j++){
-                if((mask&(1<<j))==0){
-                    dp[mask] = min(dp[mask],(v1[s]^v2[j])+solve(s+1,v1,v2,mask+(1<<j),dp));
+            for(int j=0;j<nums1.size();j++){
+                if((mask &(1<<j))==0){
+                    dp[mask]=min(dp[mask],(nums1[s]^nums2[j])+rec(s+1,mask+(1<<j),nums1,nums2));
                 }
             }
         }
         return dp[mask];
+        
     }
     int minimumXORSum(vector<int>& nums1, vector<int>& nums2) {
-        int mask = 0;
-        vector<int> dp(1<<14,INT_MAX);
-        return solve(0,nums1,nums2,mask,dp);
+        dp=vector<int>(1<<14,INT_MAX);
+        int mask=0;
+        return rec(0,mask,nums1,nums2);
     }
 };
