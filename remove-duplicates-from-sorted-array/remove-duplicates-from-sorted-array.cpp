438b7b7f31a43f6135bd0ad82c7dc413b663e1ac
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2023-07-02 17:51:42 +0530

    Time: 7 ms, Memory: 18.8 MB - LeetHub

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        set<int> st;
-        for(auto it:nums) st.insert(it);
-        vector<int> temp;
-        for(auto it:st) temp.push_back(it);
-        nums=temp;
+        vector<int> temp(200+2,0);
+        for(auto it:nums){
+            temp[it+100]++;
+        }
+        nums.clear();
+        for(int i=0;i<=201;i++){
+            if(temp[i]>0){
+                nums.push_back(i-100);
+            }
+        }
         return nums.size();
     }
 };
