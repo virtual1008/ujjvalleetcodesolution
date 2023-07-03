3056de2ca0620c5accc3276971286698bcfe961c
UJJVAL TAILOR <79957718+vijayan-0908@users.noreply.github.com>
2023-07-03 23:52:52 +0530

    Time: 27 ms, Memory: 19.1 MB - LeetHub

---
+++ move-zeroes/move-zeroes.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i=0,j=1;
+        while(j<nums.size()){
+            if(nums[i]==0){
+                if(nums[j]!=0){
+                    swap(nums[i],nums[j]);
+                    i++;
+                    j++;
+                }else{
+                    j++;
+                }
+            }else{
+                i++;
+                j++;
+            }
+        }
+    }
+};
